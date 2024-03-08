d998b8bb4b3d6544cfc61b897ed84ae908e07a4e
José Manuel Nieto <superjmn@outlook.com>
2024-03-08 13:32:50 +0100

    Improvement

--- ScrollViewerTest/EnhancedScrollViewer/ScrollBarClientAreaExpansionStrategy.cs
+++ ScrollViewerTest/EnhancedScrollViewer/ScrollBarClientAreaExpansionStrategy.cs
@@ -1,41 +1,44 @@
 ﻿using System;
 using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Avalonia.Controls.Primitives;
-using ReactiveUI;
 
 namespace ScrollViewerTest.EnhancedScrollViewer;
 
 public class ScrollBarClientAreaExpansionStrategy : ScrollViewerExpansionStrategy
 {
-    private readonly ScrollBar scrollBar;
+    private readonly CompositeDisposable disposables = new();
 
     public ScrollBarClientAreaExpansionStrategy(ScrollBar scrollBar)
     {
-        this.scrollBar = scrollBar;
-
         var isExpandedProperty = typeof(ScrollBar).GetProperty("IsExpanded");
 
-        this.WhenAnyValue(x => x.scrollBar)
-            .Select(x => Observable.FromEventPattern(x, "PointerEntered"))
-            .Switch()
-            .Do(pattern =>
-            {
-                isExpandedProperty.SetValue(scrollBar, true);
-            })
-            .Subscribe();
+        if (isExpandedProperty is null)
+        {
+            return;
+        }
 
-        this.WhenAnyValue(x => x.scrollBar)
-            .Select(x => Observable.FromEventPattern(x, "PointerExited").Delay(TimeSpan.FromSeconds(1), Scheduler.CurrentThread))
-            .Switch()
+        Observable.FromEventPattern(scrollBar, "PointerEntered")
+        .Do(_ =>
+        {
+            isExpandedProperty.SetValue(scrollBar, true);
+        })
+        .Subscribe()
+        .DisposeWith(disposables);
+
+        Observable.FromEventPattern(scrollBar, "PointerExited")
+            .Delay(TimeSpan.FromSeconds(1), Scheduler.CurrentThread)
             .Do(_ =>
             {
                 isExpandedProperty.SetValue(scrollBar, false);
             })
-            .Subscribe();
+            .Subscribe()
+            .DisposeWith(disposables);;
     }
 
     public override void Dispose()
     {
+        disposables.Dispose();
     }
 }
